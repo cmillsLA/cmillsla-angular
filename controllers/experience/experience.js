c6ab420e31b218caaf1f3191ae3da5b54fbcc2c9
chrismillsnbcu <chris.mills@nbcuni.com>
2014-10-13 15:05:13 -0700

    updates

--- controllers/experience/experience.js
+++ controllers/experience/experience.js
@@ -10,15 +10,19 @@ angular.module('portfolio.experience', ['ngRoute'])
 		if(bodyClass < 0) {
 			var fade = 'fade load';
 			var fadeDelay = 'fade-delay load';
+			var fadeDelay2 = 'fade-delay-2 load content experience';
 			$scope.appendBodyClass(' experience-loaded');
 		// Don't animate.
 		} else {
 			var fade = fadeDelay = 'load';
+			var fadeDelay2 = 'load content experience';
 		}
 		var h1 = document.getElementsByTagName('h1');
 		h1[0].className = fade;
 		var p = document.getElementsByTagName('p');
-		p[0].className = fadeDelay;	
+		p[0].className = fadeDelay;
+		var content = document.getElementsByClassName('content');
+		content[0].className = fadeDelay2;
 	}
 	// setTimeout to use CSS3 transitions.
 	setTimeout($scope.init, 0);
--- controllers/work/work.js
+++ controllers/work/work.js
@@ -10,15 +10,19 @@ angular.module('portfolio.work', ['ngRoute'])
 		if(bodyClass < 0) {
 			var fade = 'fade load';
 			var fadeDelay = 'fade-delay load';
+			var fadeDelay2 = 'fade-delay1 load content work';
 			$scope.appendBodyClass(' work-loaded');
 		// Don't animate.
 		} else {
 			var fade = fadeDelay = 'load';
+			var fadeDelay2 = 'load content work';
 		}
 		var h1 = document.getElementsByTagName('h1');
 		h1[0].className = fade;
 		var p = document.getElementsByTagName('p');
-		p[0].className = fadeDelay;	
+		p[0].className = fadeDelay;
+		var content = document.getElementsByClassName('content');
+		content[0].className = fadeDelay2;
 	}
 	// setTimeout to use CSS3 transitions.
 	setTimeout($scope.init, 0);
